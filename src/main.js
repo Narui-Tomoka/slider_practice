67d7b7241bc2e129530c5520dae0de202f174134
Tomoka Narui <sweetmelty2512@gmail.com>
2025-10-24 22:04:55 +0900

    jQueryプラグイン「slick」を使用してスライドショーを実装。ブラウザ間の表示ブレをなくすためreset.css適用

--- src/main.js
+++ src/main.js
@@ -1,1 +1,10 @@
-import "./style.css";
+$(document).ready(function () {
+  $(".slider").slick({
+    autoplay: true,
+    autoplaySpeed: 3000,
+    pauseOnHover: false,
+    arrows: true,
+    infinite: true,
+    dots: true,
+  });
+});
